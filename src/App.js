c3cd712767520ec298a604c9aed24e3b5e24f8a3
kissan-beej-bhandar <vipinrao03@gmail.com>
2022-09-11 11:19:38 +0530

    first commit2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { HashRouter as Router, Switch, Route} from "react-router-dom";
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import Home from './Pages/App/Home';
